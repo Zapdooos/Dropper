c9b220a4f1a1a757567b1636e12b5bddc883600a
Zapdooos <joel.simard@polymtl.ca>
2017-04-25 17:43:48 -0400

    commit 2

--- dropper/DropperGUI.java
+++ dropper/DropperGUI.java
@@ -189,6 +189,7 @@ public class DropperGUI extends javax.swing.JFrame {
     }// </editor-fold>                        
 
 	private void dropButtonActionPerformed(java.awt.event.ActionEvent evt) {
+		Dropper.ids.clear();
 		for (int i = 0; i < tableID.getRowCount(); i++) {
 			Integer id = Integer.valueOf(tableID.getModel().getValueAt(i, 0).toString());
 			Dropper.ids.add(id);
@@ -208,41 +209,6 @@ public class DropperGUI extends javax.swing.JFrame {
 		DefaultTableModel model = (DefaultTableModel) tableID.getModel();
 		model.addRow(new Object[] { IdText.getText() });
 	}                                   
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(DropperGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(DropperGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(DropperGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(DropperGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new DropperGUI().setVisible(true);
-            }
-        });
-    }
 
     // Variables declaration - do not modify                     
     private javax.swing.JTextField IdText;
